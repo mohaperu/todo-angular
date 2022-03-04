5b87afad332f922a1c7e3d709559975313a9af88
mohaperu <mohaperu@gmail.com>
2022-03-04 16:21:25 +0530

    push

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TodosComponent } from './todos/todos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'assign', component: TodosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const routingComponents = [TodosComponent]
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+
   title = 'todo-app';
+
+  filter: 'all' | 'active' | 'done' = 'all';
+
+  allPerson = [
+    { person: "", done: true },
+  ];
+
+  get items() { 
+    return this.allPerson.filter(item => this.filter === 'done' ? item.done : !item.done);
+  }
+
+  addItem(name: string) {  
+    
+    this.allPerson.unshift({  
+      person: name,
+      done: false
+    }); 
+  }
+
+  removeItem(name : string) {
+    this.allPerson.splice((name.length),name.length+1)
+  }
 }
